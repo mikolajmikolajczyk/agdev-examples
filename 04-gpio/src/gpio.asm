a25f6e9a74b69be73ccc3f7fc825404a6f27acbc
Mikołaj Mikołajczyk <mikolajczyk.mikolajm@gmail.com>
2024-08-02 17:32:55 +0200

    Added asm gpio example

---
+++ 04-gpio/src/gpio.asm
@@ -0,0 +1,39 @@
+assume adl=1
+
+section .text
+
+public _port_send
+public _port_recv
+
+; Function: _port_send
+; Description: Sends a byte of data to a specified port.
+; Input: (ix+6) -> port number (8-bit)
+;        (ix+9) -> data byte to be sent
+_port_send:
+    push ix            ; Save IX register
+    ld ix, 0           ; Clear IX
+    add ix, sp         ; Set IX to point to the stack frame
+    push bc            ; Save BC register
+    ld bc, 0           ; Clear BC
+    ld c, (ix+6)       ; Load port number into C
+    ld a, (ix+9)       ; Load data byte into A
+    out (bc), a        ; Output data byte to port
+    pop bc             ; Restore BC register
+    pop ix             ; Restore IX register
+    ret                ; Return from function
+
+; Function: _port_recv
+; Description: Receives a byte of data from a specified port.
+; Input: (ix+6) -> port number (8-bit)
+; Output: A register -> received data byte
+_port_recv:
+    push ix            ; Save IX register
+    ld ix, 0           ; Clear IX
+    add ix, sp         ; Set IX to point to the stack frame
+    push bc            ; Save BC register
+    ld bc, 0           ; Clear BC
+    ld c, (ix+6)       ; Load port number into C
+    in a, (bc)         ; Input data byte from port (this will be return value)
+    pop bc             ; Restore BC register
+    pop ix             ; Restore IX register
+    ret                ; Return from function
---
+++ 04-gpio/src/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "gpio.h"
+
+void delay_some()
+{
+    uint32_t count = 7;
+    uint32_t cycles_per_count = 18000; // 18,000,000 cycles per second / 1,000 milliseconds
+    uint32_t iterations_per_count = cycles_per_count / 4;
+
+    while (count--)
+    {
+        volatile uint32_t count2 = iterations_per_count;
+        while (count2--)
+        {
+            // Busy-wait loop
+        }
+    }
+}
+
+int main(void)
+{
+    printf("This example needs 8 diodes connected to PC0-7\n");
+    port_send(PC_DIR, 0x00); // set whole PC port as output
+
+    for (uint8_t i = 0; i < 255; i++)
+    {
+        port_send(PC_DAT, i);
+        printf("PC0-7: 0x%02X\n", port_recv(PC_DAT));
+        delay_some();
+    }
+    // turn off all diodes
+    port_send(PC_DAT, 0x00);
+}
